78d84111e52daa2fb4f30969a1823a804f5a21ad
chist <chistyakovhd69@gmail.com>
2025-09-26 20:48:06 +0300

    add medianOfThree to avoid the worst case

--- src/main/java/Qsort/QuickSort.java
+++ src/main/java/Qsort/QuickSort.java
@@ -7,6 +7,7 @@ import java.util.Objects;
 
 public final class QuickSort {
 
+    /// Конструктор для первого вызова
     public static <T> void quickSort(List<Integer> list, Comparator<? super Integer> cmp) {
         Objects.requireNonNull(cmp);
         if (list == null) throw new NullPointerException("list");
@@ -14,6 +15,7 @@ public final class QuickSort {
         quickSort(list, 0, list.size() - 1, cmp);
     }
 
+    /// Конструктор для рекурсии
     private static <T> void quickSort(List<T> a, int low, int high, Comparator<? super T> cmp) {
         if (low >= high) return;
         int p = partition(a, low, high, cmp);
@@ -21,8 +23,10 @@ public final class QuickSort {
         quickSort(a, p + 1, high, cmp);        // правая часть
     }
 
+    ///
     private static <T> int partition(List<T> a, int low, int high, Comparator<? super T> cmp) {
-         T pivot = a.get(high);
+        moveMedianOfThreeToHigh(a, low, high, cmp);
+        T pivot = a.get(high);
          int i = low - 1;
          for (int j = low; j < high; j++) {
            if (cmp(a.get(j), pivot, cmp) <= 0) {
@@ -33,19 +37,32 @@ public final class QuickSort {
          return i + 1;
     }
 
+    /// follow DRY
     private static <T> void swap(List<T> a, int i, int j) {
         T temp = a.get(i);
         a.set(i, a.get(j));
         a.set(j, temp);
     }
 
-    // Хелпер
+    /// Helper
     private static <T> int cmp(T x, T y, Comparator<? super T> cmp) {
         return cmp.compare(x, y);
     }
 
-    //test
+    /// To avoid the worst case
+    private static <T> void moveMedianOfThreeToHigh(List<T> a, int low, int high, Comparator<? super T> cmp) {
+        int mid = low + (high - low) / 2;
+
+        if (cmp(a.get(low), a.get(mid), cmp) > 0) swap(a, low, mid);
+        if (cmp(a.get(mid), a.get(high), cmp) > 0) swap(a, mid, high);
+        if (cmp(a.get(low), a.get(mid), cmp) > 0) swap(a, low, mid);
+
+        swap(a, mid, high);
+    }
+
+    /// Test
     public static void main(String[] args) {
+        long startTime = System.currentTimeMillis();
         List<Integer> list = new ArrayList<>();
         for (int i = 0; i < 10; i++) {
             list.add((int) (Math.random() * 100));
@@ -53,6 +70,8 @@ public final class QuickSort {
         System.out.println(list);
         Comparator<? super Integer> cmp = Integer::compareTo;
         quickSort(list, cmp);
+        long endTime = System.currentTimeMillis();
         System.out.println(list);
+        System.out.println(endTime - startTime);
     }
 }
