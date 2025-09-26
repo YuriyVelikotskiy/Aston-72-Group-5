5fe428776bdffcc605101118bed1c217041eed4a
Raichl <andfreyka.020299@gmail.com>
2025-09-26 17:27:33 +0300

    arch trouble  fix

--- src/main/java/classBuilder/Author.java
+++ src/main/java/classBuilder/Author.java
@@ -1,10 +1,8 @@
 package classBuilder;
 
 
-import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.time.LocalDate;
