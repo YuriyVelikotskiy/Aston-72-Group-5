3a3cd389461416a016c3bd838bf967703f86a8bf
chist <chistyakovhd69@gmail.com>
2025-09-30 17:49:03 +0300

    add tests for Author(hashCode, equals)

---
+++ src/test/java/AuthorTest.java
@@ -0,0 +1,30 @@
+import classBuilder.Author;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AuthorTest {
+
+    @Test
+    @DisplayName("two identical objects must be equal")
+    void shouldEqualsTwoEqualObj() {
+        Author.AuthorBuilder builder = new Author.AuthorBuilder();
+        builder.birthAYear(2000).name("Bober").country("Kurwa");
+
+        Author a1 = builder.build();
+        Author a2 = builder.build();
+        assertTrue(a1.equals(a2));
+    }
+
+    @Test
+    @DisplayName("two identical objects must have the same hash")
+    void shouldHaveSameHashCode() {
+        Author.AuthorBuilder builder = new Author.AuthorBuilder();
+        builder.birthAYear(2000).name("Bober").country("Kurwa");
+
+        Author a1 = builder.build();
+        Author a2 = builder.build();
+        assertTrue(a1.hashCode() == a2.hashCode());
+    }
+}
