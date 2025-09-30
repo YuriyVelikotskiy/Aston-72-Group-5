c6caa1ceb79bfdab153bb958c5d6efc864adff7d
Raichl <andfreyka.020299@gmail.com>
2025-09-30 21:04:50 +0300

    переписан класс записи в файл, теперь данные могут записываться по произвольному пути. есть возможность записи в режиме добавления данных. данные для записи добавляются в очередь, очередь постепенно записывается в файлы

--- src/main/java/cashCreator/CashCreates.java
+++ src/main/java/cashCreator/CashCreates.java
@@ -8,20 +8,29 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
 
-public class CashCreates implements Runnable {
+/// Запись осуществляется по средством вызова метода start(список), в этом случае берется путь по умолчанию из CASHPATH.
+/// Или start(список, путь). Тогда файл создастся по указанному пути.
+/// Файлы перезаписываются заново каждый раз.
+/// Метод start(список, путь, true) использует запись с добавлением в конец.
+/// В случае если файл не существует, то создастся новый файл
+public class CashCreates {
     private static final Path CASHPATH = Paths.get(System.getProperty("user.dir") + "\\cash");
     private static CashCreates instance;
     private Thread cashThread;
-    private ArrayList<CashedClass> pendingCacheItems;
+    private final BlockingQueue<CashTask> queue = new LinkedBlockingQueue<>();
 
-
+    /// Метод получения Instance с защитой от рефлексии
     private CashCreates() {
         if (instance != null) {
             throw new IllegalStateException("CashCreater Already exist");
         }
     }
 
+    /// Ленивая инициализация
     public static synchronized CashCreates getInstance() {
         if (instance == null) {
             return new CashCreates();
@@ -29,26 +38,96 @@ public class CashCreates implements Runnable {
         return instance;
     }
 
-
-    @Override
-    public void run() {
-        StringBuffer cash = new StringBuffer();
-        pendingCacheItems.forEach(item ->
-                cash.append(item.toJSON()).append(","));
-        cash.deleteCharAt(cash.length()-1);
-        try {
-            Files.writeString(CASHPATH,cash.toString(),StandardOpenOption.TRUNCATE_EXISTING,StandardOpenOption.CREATE );
-        }catch (IOException e){
-            throw new RuntimeException(e);
-        }
-
+    /// Метод создания кеша по умолчанию (берет путь из CASHPATH)
+    public void start(ArrayList<CashedClass> list) {
+        start(new CashTask(list,CASHPATH));
+    }
+    /// Метод создания кеша с указанием пути
+    public void start(ArrayList<CashedClass> list, Path path) {
+        start(new CashTask(list,path));
     }
 
-    public void start(ArrayList<CashedClass> pendingCacheItems) {
+    ///Метод с указанием пути и режимом добавления данных
+    public void start(ArrayList<CashedClass> list, Path path, boolean addMode) {
+        start(new CashTask(list,path).setAddMode(addMode));
+    }
+
+    private void start(CashTask task){
+        //поток создается только по необходимости
         if (cashThread == null || !cashThread.isAlive()) {
-            this.pendingCacheItems = pendingCacheItems;
-            cashThread = new Thread(this, "Cash-Thread");
+            cashThread = new Thread(this::createCash, "Cash-Thread");
             cashThread.start();
+        }
+
+        // Кладем список в очередь для выполнения
+        try {
+            queue.put(task);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /// Создание строки под запись кеша
+    private void createCash() {
+        while (!queue.isEmpty()) {
+            try {
+                CashTask task= queue.poll(100, TimeUnit.MILLISECONDS);
+                if (task != null) {
+                    addCash(task);
+                }
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                break;
+            }
+
+        }
+    }
+
+    /// Непосредственная запись в файл
+    private void addCash(CashTask task) {
+        StringBuffer cash = new StringBuffer();
+        task.getToCash().forEach(item ->
+                cash.append(item.toJSON()).append(","));
+        cash.deleteCharAt(cash.length() - 1);
+        try {
+            if (task.getAddMode()) {
+                Files.writeString(task.getPath(), cash.toString(), StandardOpenOption.APPEND, StandardOpenOption.CREATE);
+            } else {
+                Files.writeString(task.getPath(), cash.toString(), StandardOpenOption.TRUNCATE_EXISTING, StandardOpenOption.CREATE);
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    /// Задача под запись
+    private static class CashTask{
+        private final ArrayList<CashedClass> toCash;
+        private boolean addMode = false;
+        private final Path path;
+
+        public CashTask(ArrayList<CashedClass> toCash,Path path){
+            this.toCash = toCash;
+            this.path = path;
+        }
+
+
+
+        public CashTask setAddMode(boolean addMode) {
+            this.addMode = addMode;
+            return this;
+        }
+
+        public Path getPath() {
+            return path;
+        }
+
+        public boolean getAddMode() {
+            return addMode;
+        }
+
+        public ArrayList<CashedClass> getToCash() {
+            return toCash;
         }
     }
 }
