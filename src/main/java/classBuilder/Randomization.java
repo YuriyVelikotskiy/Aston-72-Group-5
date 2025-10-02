8d96594a8197c5d4631f797fcf9e0f0c08090e21
Raichl <andfreyka.020299@gmail.com>
2025-10-02 18:24:36 +0300

    Решение проблем с утечкой памяти при получении списков

--- src/main/java/classBuilder/Randomization.java
+++ src/main/java/classBuilder/Randomization.java
@@ -16,81 +16,103 @@ import java.util.Random;
 
 public class Randomization {
     /// Ссылки на файлы содержащие списки
-    private static final String DATAHOLDER = System.getProperty("user.dir") +"\\src\\randomDataHolder\\";
+    private static final String DATAHOLDER = System.getProperty("user.dir") + "\\src\\randomDataHolder\\";
     private static final String TILES = "bookTiles.txt";
-    private static final String GHENERS = "geners";
+    private static final String GENERS = "geners";
     private static final String NAMES = "names";
     private static final String COUNTRY = "country";
     private static final String PUBLISHNAMES = "publishName";
     private static final String CITY = "city";
     /// Класс рандомизации
     private static final Random RANDOM = new Random();
-    /// Получение случайного числа в диапазоне
-    private static int randomInt(int min,int max){
-        return (int) (Math.random() * (max - min + 1))+min;
+
+    private static final List<String> namesList;
+    private static final List<String> countryList;
+    private static final List<String> cityList;
+    private static final List<String> genersList;
+    private static final List<String> titleList;
+    private static final List<String> publishNameList;
+
+    static {
+        Path dataPathNames = Paths.get(DATAHOLDER + NAMES);
+        Path dataPathCountry = Paths.get(DATAHOLDER + COUNTRY);
+        Path dataPathCity = Paths.get(DATAHOLDER + CITY);
+        Path dataPathGeners = Paths.get(DATAHOLDER + GENERS);
+        Path dataPathTitle = Paths.get(DATAHOLDER + TILES);
+        Path dataPathPublisher = Paths.get(DATAHOLDER + PUBLISHNAMES);
+        try {
+            namesList = Files.readAllLines(dataPathNames);
+            countryList = Files.readAllLines(dataPathCountry);
+            cityList = Files.readAllLines(dataPathCity);
+            genersList = Files.readAllLines(dataPathGeners);
+            titleList = Files.readAllLines(dataPathTitle);
+            publishNameList = Files.readAllLines(dataPathPublisher);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
+
+    /// Получение случайного числа в диапазоне
+    private static int randomInt(int min, int max) {
+        return (int) (Math.random() * (max - min + 1)) + min;
+    }
+
     /// Получение случайной даты
-    private static int getRandomDate(){
+    private static int getRandomDate() {
         return randomInt(0, LocalDate.now().getYear());
     }
 
     /// Метод получения случайной строки по названию файла с данными
-    private static String getRandomString(String fileName){
-        Path dataPAth = Paths.get(DATAHOLDER+fileName);
-        try {
-            List<String> names = Files.readAllLines(dataPAth);
-            return names.get(RANDOM.nextInt(names.size()));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
+    private static String getRandomString(List<String> list) {
+        return list.get(RANDOM.nextInt(list.size()));
     }
+
     /// Получение случайного Автора
-    public static ArrayList<Author> getRandomAuthors(int listSize){
-        if (listSize > 0){
+    public static ArrayList<Author> getRandomAuthors(int listSize) {
+        if (listSize > 0) {
             ArrayList<Author> randomList = new ArrayList<>();
             Author.AuthorBuilder builder = new Author.AuthorBuilder();
-            for (int i = 0;i<listSize;i++){
-                randomList.add(builder.name(getRandomString(NAMES))
-                        .country(getRandomString(COUNTRY))
+            for (int i = 0; i < listSize; i++) {
+                randomList.add(builder.name(getRandomString(namesList))
+                        .country(getRandomString(countryList))
                         .birthAYear(getRandomDate()).build());
             }
             return randomList;
-        }else {
+        } else {
             throw new IllegalArgumentException("колчиество эллементов не может быть меньше 1");
         }
 
     }
 
     /// Получение случайного Издателя
-    public static ArrayList<Publisher> getRandomPublishers(int listSize){
-        if (listSize > 0){
+    public static ArrayList<Publisher> getRandomPublishers(int listSize) {
+        if (listSize > 0) {
             ArrayList<Publisher> randomList = new ArrayList<>();
             Publisher.PublisherBuilder builder = new Publisher.PublisherBuilder();
-            for (int i = 0;i<listSize;i++){
-                randomList.add(builder.name(getRandomString(PUBLISHNAMES))
-                        .city(getRandomString(CITY))
+            for (int i = 0; i < listSize; i++) {
+                randomList.add(builder.name(getRandomString(publishNameList))
+                        .city(getRandomString(cityList))
                         .foundingYear(getRandomDate()).build());
             }
             return randomList;
-        }else {
+        } else {
             throw new IllegalArgumentException("колчиество эллементов не может быть меньше 1");
         }
 
     }
 
     /// Получение случайной книги
-    public static ArrayList<Book> getRandomBooks(int listSize){
-        if (listSize > 0){
+    public static ArrayList<Book> getRandomBooks(int listSize) {
+        if (listSize > 0) {
             ArrayList<Book> randomList = new ArrayList<>();
             Book.BookBuilder builder = new Book.BookBuilder();
-            for (int i = 0;i<listSize;i++){
-                randomList.add(builder.tile(getRandomString(TILES))
-                        .genre(getRandomString(GHENERS))
+            for (int i = 0; i < listSize; i++) {
+                randomList.add(builder.tile(getRandomString(titleList))
+                        .genre(getRandomString(genersList))
                         .yearPublished(getRandomDate()).build());
             }
             return randomList;
-        }else {
+        } else {
             throw new IllegalArgumentException("колчиество эллементов не может быть меньше 1");
         }
 
