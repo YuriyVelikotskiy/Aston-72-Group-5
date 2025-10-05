926c3939593c52a241dee06de44e6e3b6953128b
Raichl <andfreyka.020299@gmail.com>
2025-10-06 00:03:04 +0300

    добавлен метод автоматического получения типов данных через рефлексию

--- src/main/java/fileReader/FileReader.java
+++ src/main/java/fileReader/FileReader.java
@@ -4,6 +4,9 @@ import classBuilder.Author;
 import classBuilder.Book;
 import classBuilder.CashedClass;
 import classBuilder.Publisher;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -32,9 +35,20 @@ public class FileReader {
             throw new IOException("ошибка чтения файла");
         }
     }
+
+    /// Метод чтения для кеша
+    public static List<? extends CashedClass> readCash(Path path)throws IOException{
+        List<String> fileData = readDataFromFile(path);
+        Class<? extends CashedClass> type = getDataType(fileData.get(0));
+        return createList(fileData,type);
+    }
+    /// Метод чтения данных из файла
+    private static List<String> readDataFromFile(Path path) throws IOException{
+        return Files.readAllLines(path);
+    }
     ///Получение данных
     private static <T extends CashedClass> List<T> getDataFromFile(Path path, Class<T> type) throws IOException {
-        List<String> fileData = Files.readAllLines(path);
+        List<String> fileData = readDataFromFile(path);
         try {
             return createList(fileData, type);
         } catch (RuntimeException e) {
@@ -42,6 +56,23 @@ public class FileReader {
         }
 
     }
+    /// Метод получения типа данных
+    private static  <T extends CashedClass> Class<T> getDataType(String elem) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode fieldName = mapper.readTree(elem);
+        if (fieldName.has(Author.class.getDeclaredFields()[0].getName())){
+            return (Class<T>) Author.class;
+        } else if (fieldName.has(Book.class.getDeclaredFields()[0].getName())) {
+            return (Class<T>)Book.class;
+        }else if (fieldName.has(Publisher.class.getDeclaredFields()[0].getName())) {
+            return (Class<T>)Publisher.class;
+        }else {
+            throw new RuntimeException("Ошибка типизации, проверьте правильность записи в файл");
+        }
+
+    }
+
+
     /// Создаем списки объектов в зависимости отт переданного класса
     private static <T extends CashedClass> List<T> createList(List<String> fileData, Class<T> type) throws IOException {
         List<T> objList = new LinkedList<>();
