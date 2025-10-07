f2b22590f51c5310e31466b4491f3b9210fdee99
chist <chistyakovhd69@gmail.com>
2025-10-07 13:36:15 +0300

    Исправлен BinarySearch + rename ReflectionGetObject

--- src/test/java/ReflectionChangeObjectTest.java
+++ src/test/java/ReflectionChangeObjectTest.java
@@ -1,15 +1,15 @@
 import classBuilder.Author;
 import org.junit.jupiter.api.Test;
 
-import static appStarter.ReflectionGetObject.reflectionGetObject;
+import static appStarter.ReflectionChangeObject.reflectionChangeObject;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class ReflectionGetObjectTest {
+public class ReflectionChangeObjectTest {
 
     @Test
     public void getObjectTest() throws IllegalAccessException {
         Author author = new Author.AuthorBuilder().country("France").name("Joe").birthAYear(1999).build();
-        reflectionGetObject(author, "country", "Jamal");
+        reflectionChangeObject(author, "country", "Jamal");
         Author author1 = new Author.AuthorBuilder().country("Jamal").name("Joe").birthAYear(1999).build();
         assertEquals(author1, author );
     }
